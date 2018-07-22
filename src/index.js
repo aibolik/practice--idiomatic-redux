327dabf8ad814c7283b4bfb4657fa6371eedd8d8
Aibol Kussain <aibolikdev@gmail.com>
2018-07-22 17:40:24 +0300

    feat: just use Provider from react-redux
    Note: look previous commit for details about
    warning of using Provider

--- src/index.js
+++ src/index.js
@@ -2,24 +2,9 @@ import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import createStore, { todoAppReducer } from './reducers/todos'
-import PropTypes from 'prop-types'
+import { Provider } from 'react-redux'
 
 const root = document.getElementsByTagName('main')[0]
-
-class Provider extends Component {
-  getChildContext() {
-    return {
-      store: this.props.store
-    }
-  }
-
-  render () {
-    return this.props.children;
-  }
-}
-Provider.childContextTypes = {
-  store: PropTypes.object
-}
 
 ReactDOM.render(
   <Provider store={createStore(todoAppReducer)}>
