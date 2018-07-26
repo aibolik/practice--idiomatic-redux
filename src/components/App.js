cbeb308d91c4af2fb2e297b9b52d010d498cc825
Aibol Kussain <aibolikdev@gmail.com>
2018-07-26 22:55:18 +0300

    refactor: extract configureStore and Root component

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import VisibleTodoList from './TodoList'
+import Footer from './Footer'
+import AddTodo from './AddTodo'
+
+const TodoApp = () => (
+  <div>
+    <AddTodo />
+    <VisibleTodoList />
+    <Footer />
+  </div>
+)
+
+export default TodoApp
--- src/index.js
+++ src/index.js
@@ -1,26 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { createStore } from 'redux'
-import { Provider } from 'react-redux'
-import App from './App'
-import todoApp from './reducers'
-import { loadState, saveState } from './localStorage'
-import throttle from 'lodash/throttle'
+import configureStore from './configureStore'
+import Root from './components/Root'
 
-const persistedState = loadState()
-
-const store = createStore(todoApp, persistedState)
-const root = document.getElementsByTagName('main')[0]
-
-store.subscribe(throttle(() => {
-  saveState({
-    todos: store.getState().todos
-  })
-}, 1000))
+const store = configureStore()
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  root
+  <Root store={store} />,
+  document.getElementsByTagName('main')[0]
 )
