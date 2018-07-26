ba682bfed34d0a1f65e45941d4c519e8d28ba044
Aibol Kussain <aibolikdev@gmail.com>
2018-07-26 21:30:26 +0300

    feat: add persisted state and uuid to new todos

--- src/index.js
+++ src/index.js
@@ -4,9 +4,19 @@ import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import App from './App'
 import todoApp from './reducers'
+import { loadState, saveState } from './localStorage'
+import throttle from 'lodash/throttle'
+
+const persistedState = loadState()
 
 const store = createStore(todoApp, persistedState)
 const root = document.getElementsByTagName('main')[0]
+
+store.subscribe(throttle(() => {
+  saveState({
+    todos: store.getState().todos
+  })
+}, 1000))
 
 ReactDOM.render(
   <Provider store={store}>
