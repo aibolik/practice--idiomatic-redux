1bfd298c2271f1a016a9d28b80adde20477eee93
Aibol Kussain <aibolikdev@gmail.com>
2018-08-01 07:20:07 +0300

    feat: fetch the Todos when route changes

--- src/api/index.js
+++ src/api/index.js
@@ -1,36 +1,33 @@
 import { v4 } from 'node-uuid'
-import { resolve } from 'upath';
 
-const fakeDatabase = {
-  todos: [
-    {
-      id: v4(),
-      text: 'Revise Redux',
-      completed: true
-    },
-    {
-      id: v4(),
-      text: 'Learn GatsbyJS',
-      completed: false
-    },
-    {
-      id: v4(),
-      text: 'Learn GraphQL',
-      completed: false
-    },
-    {
-      id: v4(),
-      text: 'Create website',
-      completed: false
-    },
-  ]
-}
+const fakeDatabase = [
+  {
+    id: v4(),
+    text: 'Revise Redux',
+    completed: true
+  },
+  {
+    id: v4(),
+    text: 'Learn GatsbyJS',
+    completed: false
+  },
+  {
+    id: v4(),
+    text: 'Learn GraphQL',
+    completed: false
+  },
+  {
+    id: v4(),
+    text: 'Create website',
+    completed: false
+  },
+]
 
-const delay = (ms) => 
+const delay = (ms) =>
   new Promise((resolve) => setTimeout(resolve, ms))
 
 export const fetchTodos = (filter) => {
-  return delay(1000).then(() => {
+  return delay(500).then(() => {
     switch (filter) {
       case 'all':
         return fakeDatabase
--- src/components/VisibleTodoList.js
+++ src/components/VisibleTodoList.js
@@ -1,16 +1,39 @@
+import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { toggleTodo } from '../actions'
 import { withRouter } from 'react-router'
 import TodoList from './TodoList'
 import { getVisibleTodos } from '../reducers'
+import { fetchTodos } from '../api'
+
+class VisibleTodoList extends Component {
+  componentDidMount() {
+    fetchTodos(this.props.filter).then(todos => {
+      console.log(this.props.filter, todos)
+    })
+  }
+
+  componentDidUpdate(prevProps) {
+    if (this.props.filter !== prevProps.filter) {
+      fetchTodos(this.props.filter).then(todos => {
+        console.log(this.props.filter, todos)
+      })
+    }
+  }
+
+  render() {
+    return <TodoList {...this.props} />
+  }
+}
 
 const mapStateToProps = (state, { match: { params: { filter } } }) => ({
-  todos: getVisibleTodos(state, filter || 'all')
+  todos: getVisibleTodos(state, filter || 'all'),
+  filter: filter || 'all'
 })
 
-const VisibleTodoList = withRouter(connect(
+VisibleTodoList = withRouter(connect(
   mapStateToProps,
   { onTodoClick: toggleTodo }
-)(TodoList))
+)(VisibleTodoList))
 
 export default VisibleTodoList
